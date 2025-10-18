0a5665c369fcfac7cd3e807b57f71e3ba0ac5dc7
mclovin2017 <galinaberestukova@gmail.com>
2025-10-18 11:10:10 +0700

    GeoFinder final version

--- Geofinder_1.py
+++ Geofinder_1.py
@@ -12,23 +12,28 @@ map = folium.Map(
     control_scale=True,
     tiles='OpenStreetMap',
     zoom_start=10.5
-)
+) # генерируем саму карту с обложкой "OpenStreetMap"
 map.add_child(folium.LatLngPopup())
 
-df1 = pd.read_csv('Rus_schools_final.csv', encoding='windows-1251')
+# принимаем нужные нам .csv файлы как дата-фреймы для дальнейшей работы с ними
+schools_data = pd.read_csv('Rus_schools_final.csv', encoding='windows-1251')
 udo = pd.read_csv('UDO.csv', encoding="utf-8", sep=';')
-schools = df1[df1['addr'].str.contains('г. Новокузнецк', na=False)]
+schools = schools_data[schools_data['addr'].str.contains('г. Новокузнецк', na=False)] # ищем школы только в городе Новокузнецк
 
+# генерируем маркер-кластеры для каждой группы объектов (учреждения доп. образования, школы подходящие по условию и школы неподходящие по условию)
 udo_cluster = MarkerCluster(name="Учреждения доп. образования").add_to(map)
 schools_near_cluster = MarkerCluster(name="Школы в радиусе 3 км от УДО").add_to(map)
 schools_far_cluster = MarkerCluster(name="Школы вне радиуса 3 км от УДО").add_to(map)
 
-added_nearby_schools = set() # список для школ подходящих по условию
-far_schools = set() # список для школ неподходящих по условию
+# генерируем списки школ подходящих и неподходящих по условию
+added_nearby_schools = set() # подходящие
+far_schools = set() # неподходящие
 
+# перебираем список учреждений доп. образования для их последующего вывода на карту
 for _, udo_row in udo.iterrows():
     udo_location = (udo_row['lat'], udo_row['lon'])
 
+    # содаём переменную для нахождения дистанции от школы до УДО
     schools_temp = schools.copy()
     schools_temp['distance'] = schools_temp.apply(
         lambda row: calculate_distance(row, udo_location),
@@ -63,63 +68,67 @@ for _, school_row in schools.iterrows():
             icon=folium.Icon(color='red', icon='graduation-cap', prefix='fa')
         ).add_to(schools_far_cluster)  # создание Marker'ов школ, не входящих в радиус 3км от УДО
 
+    # генерация каждого района на карте с помощью полигонов
     folium.Polygon(
     locations=Distrcit_Coords.polygon1,
-    popup="Новоильинский район: Плотность населения - 3450 человек на км²",
+    popup="Новоильинский район: Плотность населения - 3450 человек на км²\tрейтинг доступности УДО: 4.4/5",
     color="",
     fill=True,
     fill_color="grey",
     fill_opacity=0.009,
     tooltip = "Нажмите для подробной информации"
-).add_to(map)
+).add_to(map) # Новоильинский район
 
 folium.Polygon(
     locations=Distrcit_Coords.polygon2,
-    popup="Заводский район: Плотность населения - 848 человек на км²",
+    popup="Заводской район: Плотность населения - 848 человек на км²\tрейтинг доступности УДО: 3.8/5",
     color="",
     fill=True,
     fill_color="purple",
     fill_opacity=0.3,
     tooltip="Нажмите для подробной информации"
-).add_to(map)
-
-folium.LayerControl().add_to(map)
+).add_to(map) # Заводской район
 
 folium.Polygon(
     locations=Distrcit_Coords.polygon3,
-    popup="Кузнецкий район: Плотность населения - 1299 человек на км²",
+    popup="Кузнецкий район: Плотность населения - 1299 человек на км²\tрейтинг доступности УДО: 3.5/5",
     color="",
     fill=True,
     fill_color="red",
     fill_opacity=0.3,
     tooltip="Нажмите для подробной информации",
-).add_to(map)
+).add_to(map) # Кузнецкий район
+
 folium.Polygon(
     locations=Distrcit_Coords.polygon4,
-    popup="Центральный район: Плотность населения - 2504 человек на км²",
+    popup="Центральный район: Плотность населения - 2504 человек на км²\tрейтинг доступности УДО: 4.9/5",
     color="",
     fill=True,
     fill_color="orange",
     fill_opacity=0.3,
     tooltip="Нажмите для подробной информации"
-).add_to(map)
+).add_to(map) # Центральный район
+
 folium.Polygon(
     locations=Distrcit_Coords.polygon5,
-    popup="Орджоникидзевский район: Плотность населения - 821 человек на км²",
+    popup="Орджоникидзевский район: Плотность населения - 821 человек на км²\tрейтинг доступности УДО: 3.0/5",
     color="",
     fill=True,
     fill_color="yellow",
     fill_opacity=0.2,
     tooltip="Нажмите для подробной информации"
-).add_to(map)
+).add_to(map) # Орджоникидзевский район
+
 folium.Polygon(
     locations=Distrcit_Coords.polygon6,
-    popup="Куйбышевский район: Плотность населения - 812 человек на км²",
+    popup="Куйбышевский район: Плотность населения - 812 человек на км²\tрейтинг доступности УДО: 4.1/5",
     color="",
     fill=True,
     fill_color="darkblue",
     fill_opacity=0.3,
     tooltip="Нажмите для подробной информации"
-).add_to(map)
+).add_to(map) # Куйбышевский район
+
+folium.LayerControl().add_to(map) # добавление на карту интерактивных элементов управления, которые позволяют пользователям переключать видимость различных слоев данных
 
 map.save('map.html')
